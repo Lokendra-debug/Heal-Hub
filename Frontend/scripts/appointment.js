22798289fd4b90962e8d82dcb87bf5a6f5040466
lokendra <thelokendrasinghrathore@gmail.com>
2023-07-24 02:35:38 +0530

    ok

---
+++ Frontend/scripts/appointment.js
@@ -0,0 +1,40 @@
+async function fetchDoctorDetails(email) {
+    try {
+        const response = await fetch(`/api/doctors?email=${email}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching doctor details:", error);
+        return null;
+    }
+}
+
+
+function renderDoctorDetails(doctor) {
+    const doctorDetailsDiv = document.getElementById("doctorDetails");
+    if (!doctor) {
+        doctorDetailsDiv.innerHTML = "<h2>Doctor details not found</h2>";
+        return;
+    }
+
+    const { name, email, image, contact, specialties } = doctor;
+    doctorDetailsDiv.innerHTML = `
+        <h2>${name}</h2>
+        <img class="avatar" src="${image}" alt="Doctor Avatar">
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Contact:</strong> ${contact}</p>
+        <p><strong>Specialties:</strong> ${specialties.join(", ")}</p>
+    `;
+}
+
+
+async function fetchAndRenderDoctorAvailability(email) {
+    const doctorAvailabilityDiv = document.getElementById("doctorAvailability");
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const doctorEmail = localStorage.getItem("doctoremail");
+    const doctorDetails = await fetchDoctorDetails(doctorEmail);
+    renderDoctorDetails(doctorDetails);
+    fetchAndRenderDoctorAvailability(doctorEmail);
+});
