a47c7505141c31d16a98ba63b71e004183f42cc5
lokendra <thelokendrasinghrathore@gmail.com>
2023-07-24 01:01:34 +0530

    ok

---
+++ Frontend/scripts/adminlogin.js
@@ -0,0 +1,48 @@
+const url="http://localhost:4000/users/login"
+
+function validateForm(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    localStorage.setItem("userEmail", email);
+
+    if (!email || !password) {
+        alert('Please fill in all fields.');
+        return false;
+    }
+
+    var data = {
+        email: email,
+        password: password
+    };
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        
+        if (responseData.success) {
+            localStorage.setItem("accesstoken", responseData.accessToken);
+            localStorage.setItem("rerefreshtoken", responseData.rerefreshToken);
+            alert('Login successful!');
+        } else {
+            alert('Login failed. Please check your credentials.');
+        }
+
+        
+        
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    return true;
+}
+
+
+var loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', validateForm);
